cfe07f476365fa33f09b305103d39ce12f7517c3
David Schlechtweg <david.schlechtweg@b-s-s.de>
2013-04-16 14:42:03 +0200

    ...

--- npm-delegate.js
+++ npm-delegate.js
@@ -1,135 +1,130 @@
 #!/usr/bin/env node
-var optimist = require('optimist');
-var fallback = require('fallback');
-var http = require('http');
-var https = require('https');
-var url = require('url2');
+var optimist = require ( 'optimist' ),
+  fallback = require ( 'fallback' ),
+  http = require ( 'http' ),
+  https = require ( 'https' ),
+  url = require ( 'url2' );
 
 var argv = optimist
-  .usage('Compose multiple npm registries in fallback order.\nUsage: $0 [opts] host1/registry host2/registry ... hostN/registry')
-  .alias('p','port')
-    .default('p',5983)
-    .describe('p', 'port to listen on')
-  .alias('s','secure')
-    .default(false)
-    .boolean('s')
-    .describe('s', 'run the proxy using https?')
-  .check(function (argv) {
-    if (!argv._.length)
-      throw new Error('you must specify at least one registry (two to be useful)')
-  })
+  .usage ( 'Compose multiple npm registries in fallback order.\nUsage: $0 [opts] host1/registry host2/registry ... hostN/registry' )
+  .alias ( 'p', 'port' )
+  .default ( 'p', 5983 )
+  .describe ( 'p', 'port to listen on' )
+  .alias ( 's', 'secure' )
+  .default ( false )
+  .boolean ( 's' )
+  .describe ( 's', 'run the proxy using https?' )
+  .check ( function ( argv ) {
+  if ( !argv._.length )
+    throw new Error ( 'you must specify at least one registry (two to be useful)' )
+} )
   .argv;
 
-var registries = argv._.map(parseRegistry);
+var registries = argv._.map ( parseRegistry );
 
-function parseRegistry(string) {
-  var parsed = url.parse(string);
-  if (!/http(s?):/.test(parsed.protocol))
-    die('invalid registry address: specify a protocol (eg https://): ' + string)
+function parseRegistry ( string ) {
+  var parsed = url.parse ( string );
+  if ( !/http(s?):/.test ( parsed.protocol ) )
+    die ( 'invalid registry address: specify a protocol (eg https://): ' + string );
 
-  parsed.port || (parsed.port = 80)
+  parsed.port || (parsed.port = 80);
 
   return parsed
 }
 
-(argv.secure ? https : http).createServer(delegate).listen(argv.port)
-console.log('proxy started')
-console.dir(registries.map(url.format))
+(argv.secure ? https : http).createServer ( delegate ).listen ( argv.port );
+console.log ( 'proxy started' );
+console.dir ( registries.map ( url.format ) );
 
 
-function delegate(req, resOut) {
+function delegate ( req, resOut ) {
 
-  if (req.method !== 'GET') {
-    console.log('invalid method')
-    resOut.statusCode = 405
-    resOut.write(JSON.stringify({error: 'invalid method'}))
-    resOut.end()
+  if ( req.method !== 'GET' ) {
+    console.log ( 'invalid method' );
+    resOut.statusCode = 405;
+    resOut.write ( JSON.stringify ( {error: 'invalid method'} ) );
+    resOut.end ();
     return
   }
 
-  fallback(registries, function (registry, cb) {
-    console.log('forwarding to ' + registry.hostname)
-    forward(req, registry, function (err, res) {
-      if (!err) {
-        return cb(null, res)
+  fallback ( registries, function ( registry, cb ) {
+    console.log ( 'forwarding to ' + registry.hostname );
+    forward ( req, registry, function ( err, res ) {
+      if ( !err ) {
+        return cb ( null, res )
       }
-      console.log('err calling ' + registry.hostname + '. ' + err)
-      return cb()
-    })
+      console.log ( 'err calling ' + registry.hostname + '. ' + err );
+      return cb ()
+    } )
 
-  }, function (err, resIn, registry) {
-    if (err) {
-      resOut.write(JSON.stringify({error: 'There was an error resolving your request:\n' + JSON.stringify(err, null, 2)}))
-      return resOut.end()
+  }, function ( err, resIn, registry ) {
+    if ( err ) {
+      resOut.write ( JSON.stringify ( {error: 'There was an error resolving your request:\n' + JSON.stringify ( err, null, 2 )} ) );
+      return resOut.end ()
     }
-    else if (!resIn) {
+    else if ( !resIn ) {
       resOut.statusCode = 400
-      resOut.write(JSON.stringify({error: 'request could not be fulfilled by any of the registries on this proxy.'
-        + 'perhaps the module you\'re looking for does not exist'}))
-      resOut.end()
+      resOut.write ( JSON.stringify ( {error: 'request could not be fulfilled by any of the registries on this proxy.'
+        + 'perhaps the module you\'re looking for does not exist'} ) );
+      resOut.end ()
     } else {
-      console.log('proxying response from registry' + url.format(registry))
-      resOut.setHeader('x-registry', url.format(registry))
-      pipeRes(resIn, resOut)
+      console.log ( 'proxying response from registry' + url.format ( registry ) );
+      resOut.setHeader ( 'x-registry', url.format ( registry ) );
+      pipeRes ( resIn, resOut )
     }
-  });
+  } );
 
 
 }
 
-function forward(reqIn, registry, cb) {
+function forward ( reqIn, registry, cb ) {
   var reqOut = {
-    hostname: registry.hostname
-  , port: registry.port
-  , path: rebase(registry.path, reqIn.url)
-  , headers: reqIn.headers
-  , method: reqIn.method
-  , auth: registry.auth
-  }
-  delete reqOut.headers.host
-  delete reqOut.headers.authorization
+    hostname: registry.hostname, port: registry.port, path: rebase ( registry.path, reqIn.url ), headers: reqIn.headers, method: reqIn.method, auth: registry.auth
+  };
+  delete reqOut.headers.host;
+  delete reqOut.headers.authorization;
 
 
-  //console.log('fwd req', reqOut);
-  reqOut = (/https/.test(reqOut.protocol) ? https : http).request(reqOut, function (res) {
-    console.log('res received')
-    if (res.statusCode >= 400) {
-      console.log('bad response from ' + registry.hostname + ' ' + res.statusCode)
-      res.pipe(process.stdout)
-      return cb(new Error('Response from ' + registry.hostname + ': ' + res.statusCode))
+  console.log('fwd req', reqOut);
+  reqOut = (/https/.test ( reqOut.protocol ) ? https : http).request ( reqOut,function ( res ) {
+    console.log ( 'res received' );
+    if ( res.statusCode >= 400 ) {
+      console.log ( 'bad response from ' + registry.hostname + ' ' + res.statusCode );
+      res.pipe ( process.stdout );
+      return cb ( new Error ( 'Response from ' + registry.hostname + ': ' + res.statusCode ) )
     }
-    return cb(null, res);
-  }).on('error', cb);
-  reqOut.end();
+    return cb ( null, res );
+  } ).on ( 'error', cb );
+  reqOut.end ();
 }
 
-function pipeRes(resFrom, resTo) {
-  copyHeaders(resFrom, resTo)
-  resFrom.pipe(resTo)
+function pipeRes ( resFrom, resTo ) {
+  copyHeaders ( resFrom, resTo );
+  resFrom.pipe ( resTo )
 }
 
-function copyHeaders(resFrom, resTo) {
+function copyHeaders ( resFrom, resTo ) {
   resTo.statusCode = resFrom.statusCode;
-  for (var header in resFrom.headers) {
-    resTo.setHeader(header, resFrom.headers[header])
+  for ( var header in resFrom.headers ) {
+    resTo.setHeader ( header, resFrom.headers[header] );
   }
 }
 
 
-function rebase(pathBase, path) {
-    console.log(pathBase, path)
+function rebase ( pathBase, path ) {
+  console.log ( pathBase, path );
 
-    if (pathBase != '/registry') {
-      return path.replace('/registry','')
-    }
+  if ( pathBase != '/registry' ) {
+    return path.replace ( '/registry', '' )
+  }
 
-    if (path.indexOf('/registry') != 0) {
-      return '/registry' + path;
-    }
+  if ( path.indexOf ( '/registry' ) != 0 ) {
+    return '/registry' + path;
+  }
 }
 
 
-function die(msg) {
-  console.error(msg)
-  process.exit(1)
+function die ( msg ) {
+  console.error ( msg );
+  process.exit ( 1 );
 }
